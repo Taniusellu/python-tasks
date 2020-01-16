96aa1d805539f6fa5a33ad60ac1f4eb4f2306d78
Tatiana Moraru <tatianamoraru1998@gmail.com>
2020-01-16 00:52:23 -0600

    max/functions/methods/convert

---
+++ less4.py
@@ -0,0 +1,34 @@
+mytask = "How to calculate avarage of these grades"
+print(mytask)
+
+student_grades = [9.1, 8.8,7.5]
+
+ 
+mysum = sum(student_grades)
+length = len(student_grades)
+mean = mysum / length
+print(mean)
+
+#task1
+
+mytask1 = "Complete the script so that it prins out the maxim value of student_grades1. In the order word, your duty is to find out the appropriate function or method that returns the maximum value of a list and store its output in max_value."
+print(mytask1)
+
+student_grades1 = [9.1, 8.8, 7.5]
+max_value = max(student_grades1)
+print(max_value)
+
+
+#task2
+mytask2= "Like the previous exercise, find the proper function or method that counts how many 10.0 items there are in student_grades1 and print out the output using a print() function"
+print(mytask2)
+
+student_grades1 = [9.1, 8.8, 10.0, 7.7, 6.8, 8.0, 10.0, 8.1, 10.0, 9.9]
+print(student_grades1.count(10.0))
+
+#task3
+mytask3 = "Like the previous exercise, find the proper function or method that converts the string in username into lowercase letters and prints out the output"
+print(mytask3)
+
+username = "Python3"
+print(username.lower())
