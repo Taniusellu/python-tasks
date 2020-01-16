b641351c80d377e53aeeb91c23a5a396865e2dd0
Tatiana Moraru <tatianamoraru1998@gmail.com>
2020-01-16 00:21:23 -0600

    Variables/list

---
+++ less3.py
@@ -0,0 +1,12 @@
+mytask = "Assign a list to variables temperatures. The list should contain three items, a float, an integer, and a string."
+print(mytask)
+
+temperatures = [7.5, 9, "today"]
+print(temperatures)
+
+
+mytask1 = "Assign a list to variables rainfall. The list should contain four items, a float, an integer, a string, and a list. Yes, there's nothing wrong with lists containig other lists. Lists can contain any type."
+print(mytask1)
+
+rainfall = [19.9, 1998, "taty" ,[1,2,3,4,5]]
+print(rainfall)
