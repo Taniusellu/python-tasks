55fe8c81f59c93cf6c6589da51d9bc7105ba73bd
Tatiana Moraru <tatianamoraru1998@gmail.com>
2020-01-16 15:10:37 -0600

    Tuples/dictionary

---
+++ less6.py
@@ -0,0 +1,17 @@
+#tuples
+monday_temperatures = (1, 4, 5)
+print(monday_temperatures)
+
+
+#task1
+#Create a color_codes variable and assign a tuple to it. The tuple should contain three tuples as items.
+
+color_codes = ((4, 5, 6), (7, 8, 9), (10, 11, 12))
+print(color_codes)
+
+#task2
+#Assign a dictionary to variable day_temperatures. The dictionary should contain three keys: 'morning', 'noo'
+#and 'evening' and each key should contain a tuple as value. Each tuple shoul contain three floats.
+
+day_temperatures1 = {'morning':(1.1, 1.2, 1.3), 'noon':(1.4, 1.5, 1.6), 'evening':(1.7, 1.8, 1.9)}
+print(day_temperatures1)
