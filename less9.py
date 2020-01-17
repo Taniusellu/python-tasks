0f3a6ce58a4627d0c98df2cc3c220c18f304a33c
Tatiana Moraru <tatianamoraru1998@gmail.com>
2020-01-17 14:32:18 -0600

    Accessing and Slicing Lists and Strings

---
+++ less9.py
@@ -0,0 +1,16 @@
+#Accessing and Slicing Lists and Strings
+
+#Print out the slice ['b', 'c', 'd'] of the letters list.
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+print(letters[1:4])
+
+#Print out the slice ['a', 'b', 'c'] of the letters list
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+print(letters[0:3])
+
+
+#Print out the slice ['e', 'f', 'g'] of the letters list.
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+print(letters[4:])  #or use the command print(letters[-3:])
+
