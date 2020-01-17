ddbc41fbdbf8c914267e79fc679f18b87af74ef3
Tatiana Moraru <tatianamoraru1998@gmail.com>
2020-01-17 13:48:54 -0600

    Append/Remove methods into lists

---
+++ less7.py
@@ -0,0 +1,19 @@
+#Append the value of current to the end of the list seconds. Please use the list.apend() method to do that.
+seconds = [1.1234566, 1.543356, 1.23567777, 1.8754688]
+current = 1.59395524
+seconds.append(current)
+print(seconds)
+
+#Remove item 1.1234566 from seconds
+seconds = [1.1234566, 1.543356, 1.23567777, 1.8754688]
+current = 1.1234566
+seconds.remove(current)
+print(seconds)
+
+#Remove all items from seconds
+seconds = [1.1234566, 1.543356, 1.23567777, 1.8754688]
+seconds.remove(1.1234566)
+seconds.remove(1.543356)
+seconds.remove(1.23567777)
+seconds.remove(1.8754688)
+print(seconds)
