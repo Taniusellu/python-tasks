85e52e4e685f0f416203601062c19b5ac031ae3f
Tatiana Moraru <tatianamoraru1998@gmail.com>
2020-01-17 14:07:42 -0600

    Extra tasks about append method

---
+++ less8.py
@@ -0,0 +1,19 @@
+#Complete the script so that prints out the 3rd item of list serials.
+
+serials = ["RH80810A", "AA899819A", "XYSA9099400", "OOP8988459", "EEO8904882", "KOC9889482"]
+print(serials[2])
+
+#Complete the script so that it prints out the 1st, the 3rd, and the 6th items of list serials!
+
+serials = ["RH80810A", "AA899819A", "XYSA9099400", "OOP8988459", "EEO8904882", "KOC9889482"]
+print(serials[0])
+print(serials[2])
+print(serials[5])
+#Or use the command   print(serials[0], serials[2], serials[5]) 
+
+
+#Append the first item of weekend to workdays
+workdays = workdays = ["Mon", "Tue", "Wed", "Thu", "Fri"]
+weekend = ["Sat", "Sun"]
+workdays.append(weekend[0])
+print(workdays)
